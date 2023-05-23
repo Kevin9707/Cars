c5ec4d05b9cbee331e23d74f002b296d5d23d4f5
Kevin9707 <113577031+Kevin9707@users.noreply.github.com>
2023-05-23 12:54:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import cv2
 import numpy as np
 
-# capturing or reading video
-#cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('traffic.mp4')
-
-# adjusting frame rate
 fps = cap.set(cv2.CAP_PROP_FPS,1)
 
+min_contour_width=40  
 
-# minimum contour width
-min_contour_width=40  #40
-
-# minimum contour height
-min_contour_height=40  #40
-offset=10   #10
-line_height=550  #550
+min_contour_height=40
+offset=10
+line_height=550
 matches =[]
 cars=0
 
-# defining a function
 def get_centroid(x, y, w, h):
 
     x1 = int(w / 2)
@@ -51,7 +43,6 @@ while ret:
     dilated = cv2.dilate(th,np.ones((3,3)))
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
 
-    # Fill any small holes
     closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
     contours,h = cv2.findContours(closing,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for(i,c) in enumerate(contours):
@@ -77,16 +68,12 @@ while ret:
     cv2.putText(frame1, "Total Vehicles Detected: " + str(cars), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1,
                 (0, 170, 0), 2)
 
-    #cv2.drawContours(frame1,contours,-1,(0,0,255),2)
-
 
     cv2.imshow("OUTPUT" , frame1)
-    #cv2.imshow("Difference" , th)
     if cv2.waitKey(1) == 27:
         break
     frame1 = frame2
     ret , frame2 = cap.read()
 
-#print(matches)
 cv2.destroyAllWindows()
 cap.release()
